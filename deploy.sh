ac0c77c0d2d920215381f7b670feb41ada8024e9
Yehuda Tuvia <ytuvia@gmail.com>
2019-06-04 22:20:42 +0300

    setup local builds

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,27 @@
 set -e
 SERVER_ADDRESS=31.168.173.124
 SERVER_USER=pi
-
+rm -rf /etc/RFoptic/*
 echo "archiving projects and uploading"
 rm -f rfof-common.zip
 zip -r --exclude="*.git*" --exclude="*node_modules*" --exclude="*dist*" --exclude=".*" --exclude="/.*" rfof-common.zip .
-scp rfof-common.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
-scp expand.sh $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+#scp rfof-common.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+#scp expand.sh $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+sudo cp rfof-common.zip /etc/RFoptic
 
 cd ../rfof-client
 rm -f rfof-client.zip
 zip -r --exclude="*.git*" --exclude="*node_modules*" --exclude="*dist*" --exclude="assets*" --exclude="e2e*" --exclude=".*" --exclude="/.*" rfof-client.zip .
-scp rfof-client.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+#scp rfof-client.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+sudo cp rfof-client.zip /etc/RFoptic 
 
 cd ../rfof-server
 rm -f rfof-server.zip
 zip -r --exclude="*.git*" --exclude="*node_modules*" --exclude="*dist*" --exclude=".*" --exclude="/.*" rfof-server.zip .
-scp rfof-server.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+#scp rfof-server.zip $SERVER_USER@$SERVER_ADDRESS:/etc/RFoptic
+sudo cp rfof-server.zip /etc/RFoptic 
 
 echo "executing expand script on server"
 cd ../rfof-common
-ssh $SERVER_USER@$SERVER_ADDRESS "bash -s" < "expand.sh"
+./expand.sh
+#ssh $SERVER_USER@$SERVER_ADDRESS "bash -s" < "expand.sh"
--- expand.sh
+++ expand.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
+SERVER_ADDRESS=31.168.173.124
+SERVER_USER=pi
 cd /etc/RFoptic
 
 unzip -o rfof-common.zip -d /etc/RFoptic/rfof-common
 unzip -o rfof-server.zip -d /etc/RFoptic/rfof-server
 unzip -o rfof-client.zip -d /etc/RFoptic/rfof-client
 
-mv rfof-common.zip /home/pi/projects/html-server
-mv rfof-server.zip /home/pi/projects/html-server
-mv rfof-client.zip /home/pi/projects/html-server
+#mv rfof-common.zip /home/pi/projects/html-server
+#mv rfof-server.zip /home/pi/projects/html-server
+#mv rfof-client.zip /home/pi/projects/html-server
 
 echo "install rfof-common package"
 cd /etc/RFoptic/rfof-common
@@ -34,15 +36,19 @@ rm -rf *.*
 rm -rf node_modules/
 rm -rf src
 
-cd /etc/RFoptic/rfof-common
-sudo cp rfof-server-service.service /lib/systemd/system
+#cd /etc/RFoptic/rfof-common
+#sudo cp rfof-server-service.service /lib/systemd/system
 
+#/home/pi/projects/etcRFoptic.gz
 cd /etc/RFoptic/
-sudo rm -rf /home/pi/projects/etcRFoptic.gz
-sudo tar -czf /home/pi/projects/etcRFoptic.gz rfof-common/ rfof-client/ rfof-server/
+now="$(date +'%d%m%Y')"
+file="etcRFoptic${now}.gz"
+sudo rm -rf $file
+sudo tar -czf $file rfof-common/ rfof-client/ rfof-server/
 
-sudo systemctl daemon-reload
-sudo systemctl stop rfof-server-service
-sudo systemctl start rfof-server-service
+scp $file $SERVER_USER@$SERVER_ADDRESS:/home/pi/projects/
+#sudo systemctl daemon-reload
+#sudo systemctl stop rfof-server-service
+#sudo systemctl start rfof-server-service
 
 
